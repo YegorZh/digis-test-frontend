bf1b080c5a8617894ce553b94d7a892659f04c81
Yegor <yegorzhukovskiya@gmail.com>
2022-03-23 11:49:24 +0200

    Preset created

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './reset.css';
+
+ReactDOM.render(<h1>Hello Simple React App!</h1>, document.getElementById('root'));
